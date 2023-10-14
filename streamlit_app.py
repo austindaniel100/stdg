6ca6644d8328eb230e54d0d2ed4da50027104840
Austin <austindaniel100@gmail.com>
2023-10-14 16:19:30 -0400

    j

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,11 @@ st.markdown(
             font-size: 20px;
             padding: 10px 20px;
             margin: 5px;
+            display: inline-block;
+            background-color: #f63366;
+            color: white;
+            border: none;
+            cursor: pointer;
         }
     </style>
     """,
@@ -28,10 +33,27 @@ st.title('Disc Golf Shots Tracker')
 
 # Function to display button and update counts
 def display_button(shot_type):
-    # Use custom CSS class for larger buttons
-    if st.button(shot_type, key=shot_type, class_name="big-btn"):
+    # Use custom HTML for larger buttons
+    button_html = f'<button class="big-btn" id="{shot_type}">{shot_type}</button>'
+    pressed = st.markdown(button_html, unsafe_allow_html=True)
+
+    if st.session_state.get(shot_type, False):
         st.session_state.button_counts[shot_type] += 1
+        st.session_state[shot_type] = False
+
     st.write(f"{st.session_state.button_counts[shot_type]}")
+
+    # Use a script to detect button press and update session state
+    st.markdown(
+        f"""
+        <script>
+            document.getElementById("{shot_type}").onclick = function() {{
+                fetch("/streamlit-v1/SessionState/{shot_type}", {{method: "POST"}});
+            }};
+        </script>
+        """,
+        unsafe_allow_html=True
+    )
 
 # Display each button
 for shot_type in st.session_state.button_counts.keys():
