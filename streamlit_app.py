5c9b65bf513e96150195f47df823b2772083e1f3
Austin <austindaniel100@gmail.com>
2023-10-14 16:28:59 -0400

    l

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,44 @@
 import streamlit as st
 
-# Check if 'button_counts' and 'total_throws' are already in the session state
-if 'button_counts' not in st.session_state:
-    st.session_state.button_counts = {
-        "Ace": 0,
-        "Bullseye": 0,
-        "C1 X": 0,
-        "C2": 0,
-        "Other": 0
+# Cached function to initialize or get the counts
+@st.cache(allow_output_mutation=True, persist=True)
+def get_data():
+    return {
+        "button_counts": {
+            "Ace": 0,
+            "Bullseye": 0,
+            "C1 X": 0,
+            "C2": 0,
+            "Other": 0
+        },
+        "total_throws": 0
     }
-if 'total_throws' not in st.session_state:
-    st.session_state.total_throws = 0
+
+data = get_data()
 
 st.title('Disc Golf Shots Tracker')
 
 # Function to display button and update counts
 def display_button(shot_type):
     if st.button(shot_type):
-        st.session_state.button_counts[shot_type] += 1
-        st.session_state.total_throws += 1
-    st.write(f"{st.session_state.button_counts[shot_type]}")
+        data["button_counts"][shot_type] += 1
+        data["total_throws"] += 1
+    st.write(f"{data['button_counts'][shot_type]}")
 
 # Display each button
-for shot_type in st.session_state.button_counts.keys():
+for shot_type in data["button_counts"].keys():
     display_button(shot_type)
 
 # Display total throws after all buttons have been processed
-st.write(f"Total Throws: {st.session_state.total_throws}")
+st.write(f"Total Throws: {data['total_throws']}")
+
+# Reset button
+if st.button("Reset"):
+    data["button_counts"] = {
+        "Ace": 0,
+        "Bullseye": 0,
+        "C1 X": 0,
+        "C2": 0,
+        "Other": 0
+    }
+    data["total_throws"] = 0
