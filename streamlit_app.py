e9476452a4417b1f004d08d8ed3a5012361976cd
Austin <austindaniel100@gmail.com>
2023-10-14 16:16:04 -0400

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,23 @@
 import streamlit as st
 
-# Initialize dictionary to keep track of button presses
-button_counts = {
-    "Ace": 0,
-    "Bullseye": 0,
-    "C1 X": 0,
-    "C2": 0,
-    "Other": 0
-}
+# Check if 'button_counts' is already in the session state
+if 'button_counts' not in st.session_state:
+    st.session_state.button_counts = {
+        "Ace": 0,
+        "Bullseye": 0,
+        "C1 X": 0,
+        "C2": 0,
+        "Other": 0
+    }
 
 st.title('Disc Golf Shots Tracker')
 
 # Function to display button and update counts
 def display_button(shot_type):
     if st.button(shot_type):
-        button_counts[shot_type] += 1
-    st.write(f"{button_counts[shot_type]}")
+        st.session_state.button_counts[shot_type] += 1
+    st.write(f"{st.session_state.button_counts[shot_type]}")
 
 # Display each button
-for shot_type in button_counts.keys():
+for shot_type in st.session_state.button_counts.keys():
     display_button(shot_type)
