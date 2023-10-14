382889cbc457b1c8cce239b6870c7d6a7b34f215
Austin <austindaniel100@gmail.com>
2023-10-14 16:17:41 -0400

    hi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
+# Custom CSS for larger buttons
+st.markdown(
+    """
+    <style>
+        .big-btn {
+            font-size: 20px;
+            padding: 10px 20px;
+            margin: 5px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Check if 'button_counts' is already in the session state
 if 'button_counts' not in st.session_state:
     st.session_state.button_counts = {
@@ -14,7 +28,8 @@ st.title('Disc Golf Shots Tracker')
 
 # Function to display button and update counts
 def display_button(shot_type):
-    if st.button(shot_type):
+    # Use custom CSS class for larger buttons
+    if st.button(shot_type, key=shot_type, class_name="big-btn"):
         st.session_state.button_counts[shot_type] += 1
     st.write(f"{st.session_state.button_counts[shot_type]}")
 
